f938e6bc4e3a14f8051a5179007d547df8c2653e
Vincent DEBANDE <vincent.debande@devinci.fr>
2021-03-23 15:36:30 +0100

    :rocket: implement filters based on intents | TBD : Query to API #100

--- fashionBot/fashion/index.js
+++ fashionBot/fashion/index.js
@@ -19,26 +19,6 @@ const extractEntity = (nlp, entity) => {
   }
 };
 
-// const getProducts = async function(filter) {
-//   url = apiURL;
-//   axios.get(url).then(res => {
-//     products = res.data.result.map(p => {
-//       return {
-//         'name': p.nameP,
-//         'price': p.price,
-//         'image': p.images[0],
-//         'color': p.color,
-//         'material': p.material,
-//         'onSale': p.onSale,
-//         'link': p.link
-//       }
-//     });
-
-//     console.log("getProducts: ", products.length);
-//     return products;
-//   })
-// }
-
 const getProducts = () => {
   return new Promise(async (resolve, reject) => {
     url = apiURL;
@@ -56,8 +36,6 @@ const getProducts = () => {
           'type': p.categories[1]
         }
       });
-
-      console.log("getProducts: ", products.length);
       resolve(products);
     }).catch((err) => {
       reject(err.response.data);
@@ -65,18 +43,47 @@ const getProducts = () => {
   });
 }
 
-
-
 module.exports = (nlpData) => {
   return new Promise(async (resolve, reject) => {
     let intent = extractEntity(nlpData, "intent");
+    let entities = nlpData.entities;
+
+    let filter = {};
+    
     if (intent == 'getProducts') {
+
+      // If no entities 
       if (Object.keys(nlpData.entities).length == 0) {
         // We get 3 randoms products
         products = await getProducts(null);
         resolve({ 'products': products });
       }
+
+      else {
+        // If there is a price we get the price and the type of filter (more or less)
+        if(entities.hasOwnProperty('price:price')){
+          filter['price'] = parseInt(extractEntity(nlpData, 'price')); 
+          filter['filterPrice'] = extractEntity(nlpData, 'filterPrice');
+        }
+
+        if(entities.hasOwnProperty('sale:sale')){
+          let res = false;
+          let value = extractEntity(nlpData, 'sale');
+          if(value === 'true') res = true;
+
+          filter['onSale'] = res;
+        }
+
+        if(entities.hasOwnProperty('color:color')){
+
+          filter['color'] = extractEntity(nlpData, 'color');
+        }
+        if(entities.hasOwnProperty('material:material')){
+          filter['material'] = extractEntity(nlpData, 'material');
+        }
+
+        console.log("Here is the filter: ", filter);
+      }
     }
-    //resolve({"intent": intent});
   });
 };
